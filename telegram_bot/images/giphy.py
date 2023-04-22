be3c92fdd82ac94b8761b079a98127ff6a22865f
Konstantin Motuskov <motuskov@gmail.com>
2023-04-22 18:21:40 -0300

    Implement a module for work with the Giphy service

---
+++ telegram_bot/images/giphy.py
@@ -0,0 +1,65 @@
+'''
+Represents functions for getting images from the Internet.
+'''
+import aiohttp
+
+
+URL = 'https://api.giphy.com/v1/gifs/random'
+
+class ServiceUnavailable(Exception):
+    '''
+    Represents an exception that means the Giphy service hasn't answered or returned some
+    error.
+    '''
+    pass
+
+class AccessDenied(Exception):
+    '''
+    Represents an exception that means the Giphy service has returned code 401.
+    '''
+    pass
+
+async def get_random_image_by_tag(tag: str, api_key: str) -> bytes:
+    '''
+    Retrieves a random image from the Giphy service.
+    '''
+    # Set request parameters
+    params = {
+        'tag': tag,
+        'api_key': api_key
+    }
+
+    # Get an image URL
+    async with aiohttp.ClientSession() as session:
+        async with session.get(URL, params=params) as response:
+            # Get response data
+            try:
+                data = await response.json()
+            except:
+                raise ServiceUnavailable('Bad or absent JSON data in the response.')
+
+            # Get the image URL
+            if response.status == 200:
+                try:
+                    image_url = data['data']['images']['downsized_large']['url']
+                except KeyError:
+                    raise ServiceUnavailable('Bad data has received from the service.')
+            elif response.status == 401:
+                try:
+                    message = data['meta']['msg']
+                except KeyError:
+                    message = 'There is no any message in the service response.'
+                raise AccessDenied(message)
+            else:
+                raise ServiceUnavailable(f'Status code: {response.status}')
+
+    # Download the image
+    async with aiohttp.ClientSession() as session:
+        async with session.get(image_url) as response:
+            if response.status == 200:
+                try:
+                    return await response.read()
+                except:
+                    raise ServiceUnavailable('Some bad image data has received.')
+            else:
+                raise ServiceUnavailable(f'Status code: {response.status}')
---
+++ telegram_bot/images/test_giphy.py
@@ -0,0 +1,31 @@
+import asyncio
+import pytest
+
+from . import giphy
+
+
+TEST_API_KEY = 'E4uw37nYmIMKByCuNZqEkCDlHUopYXlW'
+
+def test_get_random_image_by_tag_output():
+    '''
+    Tests output of 'get_random_image_by_tag' function.
+    '''
+    image = asyncio.run(giphy.get_random_image_by_tag('funny', TEST_API_KEY))
+    assert len(image) > 0
+
+def test_get_random_image_by_tag_bad_api_key():
+    '''
+    Tests 'get_random_image_by_tag' function with bad API key given.
+    '''
+    with pytest.raises(giphy.AccessDenied):
+        asyncio.run(giphy.get_random_image_by_tag('funny', '_'))
+
+def test_get_random_image_by_tag_bad_url():
+    '''
+    Tests 'get_random_image_by_tag' function with bad URL.
+    '''
+    url = giphy.URL
+    giphy.URL = 'https://api.giphy.com/v1/gifs/___'
+    with pytest.raises(giphy.ServiceUnavailable):
+        asyncio.run(giphy.get_random_image_by_tag('funny', TEST_API_KEY))
+    giphy.URL = url
