4151c995c112ad7a29f8e76b6e2d638e9a5d43de
Konstantin Motuskov <motuskov@gmail.com>
2023-04-23 13:18:03 -0300

    Implement functionality for creating polls

--- telegram_bot/telegram_bot.py
+++ telegram_bot/telegram_bot.py
@@ -11,6 +11,10 @@ from aiogram.dispatcher.filters.state import (
     StatesGroup,
 )
 from aiogram.dispatcher import FSMContext
+from aiogram.types import (
+    InlineKeyboardMarkup,
+    InlineKeyboardButton,
+)
 
 from weather import openweather
 from exchangerates import exchangerates
@@ -30,6 +34,7 @@ MENU = (
     '  /weather - to check current weather in a given locality\n'
     '  /currencies - to convert currencies\n'
     '  /funny - to get a funny image\n'
+    '  /poll - to create a poll\n'
     '  /cancel - to cancel a command process'
 )
 
@@ -39,6 +44,9 @@ logging.basicConfig(level=logging.INFO)
 # Initialize bot and dispatcher
 bot = Bot(token=TELEGRAM_BOT_API_TOKEN)
 dp = Dispatcher(bot, storage=MemoryStorage())
+
+# Initialize a storage for keeping the list of group chats
+group_chats = {}
 
 # States
 class WeatherStates(StatesGroup):
@@ -54,6 +62,14 @@ class CurrenciesStates(StatesGroup):
     from_currency = State()
     to_currency = State()
     amount = State()
+
+class PollStates(StatesGroup):
+    '''
+    Represents a group of states for creating a poll.
+    '''
+    group_chat = State()
+    question = State()
+    answer = State()
 
 # Message handlers
 @dp.message_handler(state='*', commands=['cancel'])
@@ -238,6 +254,130 @@ async def funny_image(message: types.Message):
         await message.answer(
             'Something went wrong. We\'re working on that. Try again later.'
         )
+
+@dp.message_handler(commands=['poll'])
+async def poll_start(message: types.Message):
+    '''
+    Starts polls creator.
+    '''
+    # Check if there are any group chats where the bot is present
+    if not group_chats:
+        await message.answer(
+            'There are no active group chats I\'m involved in. Add me to a group chat and try '
+            'again.'
+        )
+        return
+
+    # Set initial state
+    await PollStates.group_chat.set()
+
+    # Ask user to choose a chat group
+    answer_buttons = [InlineKeyboardButton(group_chat[1], callback_data=group_chat[0])
+               for group_chat in group_chats.items()]
+    answer_buttons.append(InlineKeyboardButton('There are no desired chat in the list', callback_data=0))
+    answer_markup = InlineKeyboardMarkup(row_width=1)
+    answer_markup.add(*answer_buttons)
+    await message.answer('Choose a group chat where you want to start a poll:', reply_markup=answer_markup)
+
+@dp.callback_query_handler(state=PollStates.group_chat)
+async def poll_group_chat(callback_query: types.CallbackQuery, state: FSMContext):
+    '''
+    Processes selected group chat.
+    '''
+    # Check selected group chat ID
+    if callback_query.data == '0' :
+        await callback_query.message.answer(
+            'If a desired group chat isn\'t in the list you need to add the bot to the desired '
+            'group chat. If the bot is in the desired grout chat try to remove it from the chat '
+            'and add it again.'
+        )
+        await state.finish()
+        return
+
+    # Save selected group chat ID
+    async with state.proxy() as data:
+        data['group_chat_id'] = callback_query.data
+
+    # Go to the next state
+    await PollStates.next()
+
+    # Ask user to put a poll question
+    await callback_query.message.answer('What question would you like to ask in the poll?')
+
+@dp.message_handler(state=PollStates.question)
+async def poll_question(message: types.Message, state: FSMContext):
+    '''
+    Processes poll question.
+    '''
+    # Save given poll question
+    async with state.proxy() as data:
+        data['question'] = message.text
+
+    # Go to the next state
+    await PollStates.next()
+
+    # Ask user to put a poll answer
+    await message.answer('Enter a posible answer of the poll.')
+
+@dp.message_handler(state=PollStates.answer)
+async def poll_answer(message: types.Message, state: FSMContext):
+    '''
+    Processes poll answer.
+    '''
+    # Save given poll answer
+    async with state.proxy() as data:
+        answers = data.get('answers', [])
+        answers.append(message.text)
+        data['answers'] = answers
+
+    # Ask user to put a poll answer
+    if len(answers) < 2:
+        await message.answer('Enter a next posible answer of the poll.')
+    else:
+        answer_buttons = [InlineKeyboardButton('finish', callback_data='finish')]
+        answer_markup = InlineKeyboardMarkup(row_width=1)
+        answer_markup.add(*answer_buttons)
+        await message.answer(
+            'Enter a next posible answer of the poll or finish creating the poll.',
+            reply_markup=answer_markup
+        )
+
+@dp.callback_query_handler(
+        lambda callback_query: callback_query.data == 'finish',
+        state=PollStates.answer
+)
+async def poll_group_chat(callback_query: types.CallbackQuery, state: FSMContext):
+    '''
+    Creates a poll.
+    '''
+    async with state.proxy() as data:
+        # Create a poll
+        await bot.send_poll(
+            data['group_chat_id'],
+            data['question'],
+            data['answers']
+        )
+
+        # Answer the user
+        await callback_query.message.answer('I\'ve sent the poll to chosen group chat.')
+
+        # Finish state
+        await state.finish()
+
+@dp.my_chat_member_handler()
+async def manage_group_chat_list(my_chat_member: types.ChatMemberUpdated):
+    '''
+    Manages the list of group chats where the bot is present.
+    '''
+    # Return if the chat isn't a group
+    if my_chat_member.chat.type != 'group':
+        return
+
+    # Manage the list of group chats
+    if my_chat_member.new_chat_member.status == 'left':
+        group_chats.pop(my_chat_member.chat.id, None)
+    else:
+        group_chats[my_chat_member.chat.id] = my_chat_member.chat.title
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
