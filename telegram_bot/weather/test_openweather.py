091ba00cf7d4ed13061a619e9289fd2baeb3c41a
Konstantin Motuskov <motuskov@gmail.com>
2023-04-23 18:05:16 -0300

    Refactor the weather module

--- telegram_bot/weather/test_openweather.py
+++ telegram_bot/weather/test_openweather.py
@@ -1,3 +1,6 @@
+'''
+Tests 'openweather' module.
+'''
 import asyncio
 import pytest
 
@@ -7,6 +10,9 @@ from . import openweather
 TEST_API_KEY = 'b5ea7a64dc1c5450e68c809c8fda159b'
 
 def test_get_locality_weather_output():
+    '''
+    Tests the output of 'get_locality_weather' function.
+    '''
     boston_weather = asyncio.run(
         openweather.get_locality_weather('Boston', TEST_API_KEY)
     )
@@ -17,24 +23,34 @@ def test_get_locality_weather_output():
     assert 0 < boston_weather['humidity']['value'] < 100
     assert boston_weather['humidity']['units'] == '%'
 
-def test_get_locality_weather_exceptions():
-    # Bad locality name
+def test_get_locality_weather_bad_locality():
+    '''
+    Tests raising exception in case of a non-existent locality given.
+    '''
+    locality_name = '___'
     with pytest.raises(openweather.UnknownLocality):
         asyncio.run(
-            openweather.get_locality_weather('___', TEST_API_KEY)
+            openweather.get_locality_weather(locality_name, TEST_API_KEY)
         )
 
-    # Bad API_KEY
+def test_get_locality_weather_bad_api_key():
+    '''
+    Tests raising exception in case of an invalid API key given.
+    '''
+    api_key = '___'
     with pytest.raises(openweather.AccessDenied):
         asyncio.run(
-            openweather.get_locality_weather('Boston', '___')
+            openweather.get_locality_weather('Boston', api_key)
         )
 
-    # Service is not available
-    base_url = openweather.BASE_URL
-    openweather.BASE_URL = 'https://api.openweathermap.org/data/2.5/___'
+def test_get_locality_weather_bad_url():
+    '''
+    Tests raising exception during imitation of service unavailability.
+    '''
+    url = openweather.URL
+    openweather.URL = 'https://api.openweathermap.org/data/2.5/___'
     with pytest.raises(openweather.ServiceUnavailable):
         asyncio.run(
             openweather.get_locality_weather('Boston', TEST_API_KEY)
         )
-    openweather.BASE_URL = base_url
+    openweather.URL = url
